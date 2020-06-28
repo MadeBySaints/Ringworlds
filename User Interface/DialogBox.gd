bff832065b39763c9508f4cfb3bd324eef58d96f
Chris Hunsinger <hunsinger89@gmail.com>
2020-06-28 18:40:46 -0400

    multiple misc updates

---
+++ User Interface/DialogBox.gd
@@ -0,0 +1,22 @@
+extends Control
+
+#Properties for Label Node
+#Stringtext [default: ""]
+#Alignalign [default: 0]
+#VAlignvalign [default: 0]
+#boolautowrap [default: false]
+#boolclip_text [default: false]
+#booluppercase [default: false]
+#intvisible_characters [default: -1]
+#floatpercent_visible [default: 1.0]
+#intlines_skipped [default: 0]
+#intmax_lines_visible [default: -1]
+#MouseFiltermouse_filter [override: 2]
+#intsize_flags_vertical [override: 4]
+
+export var speaker : String
+
+
+#handle no speaker
+func _ready():
+	pass
