948314cc3babe4baefa605ad1bcc379fcb0200ce
MadeBySaints <MadeBySaints@users.noreply.github.com>
2020-09-20 03:00:51 -0400

    flicker modification still wip

--- World/Prop Scenes/TorchLight.gd
+++ World/Prop Scenes/TorchLight.gd
@@ -1,24 +1,43 @@
-extends Light2D
+extends Area2D
 
-export var energyMaximum : float = 1.0
-export var energyMinimum : float = 0.1
-onready var cframe = get_parent().get_node("AnimatedSprite").get_frame()
-onready var cenergy = get_parent().get_node("Light2D").get_energy()
+export var emax : float = 1.0
+export var emin : float = 0.2
+onready var sframe = $AnimatedSprite.get_frame()
+onready var cenergy = $Light2D.get_energy()
+var e = Light2D.new()
 
-func _process(delta):
-	print(cframe)
-	if cframe == 0:
-		energy = energyMinimum
-	elif cframe == 1:
-		energy = 0.3
-	elif cframe == 2:
-		energy = 0.6
-	elif cframe == 3:
-		energy = energyMaximum
-	elif cframe == 4:
-		energy = 0.6
-	elif cframe == 5:
-		energy = 0.3
+enum hue {
+	RED,
+	ORANGE,
+	YELLOW,
+	GREEN,
+	BLUE,
+	INDIGO,
+	VIOLET,
+	WHITE,
+	CYCLE
+}
+
+#func _process(_delta):
 	
-#func _on_AnimatedSprite_frame_changed():
-#
+func _on_AnimatedSprite_frame_changed():
+	print(sframe)
+	if sframe == 0:
+		print(sframe)
+		$Light2D.set_energy(emin)
+	if sframe == 1:
+		print(sframe)
+		$Light2D.set_energy(0.3)
+	if sframe == 2:
+		print(sframe)
+		$Light2D.set_energy(0.6)
+	if sframe == 3:
+		print(sframe)
+		$Light2D.set_energy(rand_range(0.8, emax))
+	if sframe == 4:
+		print(sframe)
+		$Light2D.set_energy(0.6)
+	if sframe == 5:
+		print(sframe)
+		$Light2D.set_energy(0.3)
+
