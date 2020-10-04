c347d7e629175bbe4f0100c537817c92add92b85
MadeBySaints <MadeBySaints@users.noreply.github.com>
2020-10-04 04:04:32 -0400

    minor updates to the torch and its flame

--- Core/Scripts/Spin.gd
+++ Core/Scripts/Spin.gd
@@ -1,4 +1,4 @@
-extends Area2D
+extends Node
 
 onready var anim = get_node("AnimatedSprite")
 
@@ -12,7 +12,6 @@ func rot():
 		anim.set_rotation_degrees(anim.rotation_degrees + 1)
 	elif anim.rotation_degrees >= 359.9:
 		anim.set_rotation_degrees(-360)
-	print(anim.rotation_degrees)
 	
 func _process(_delta):
 	rot()
