32f1eaebc4a78c6361574ee84276ec2d54002b7c
MadeBySaints <MadeBySaints@users.noreply.github.com>
2020-10-05 05:01:57 -0400

    moved a couple files
    added fences and set up canvas modulate rules WIP

--- Core/Scripts/CanvasModulate.gd
+++ Core/Scripts/CanvasModulate.gd
@@ -1,9 +1,36 @@
-extends Node2D
+extends CanvasModulate
+
+var col = get_color()
+
+enum colors {
+	FULLBRIGHT,
+	DUSK,
+	DAWN,
+	RED,
+	ORANGE,
+	YELLOW,
+	GREEN,
+	BLUE,
+	INDIGO,
+	VIOLET
+}
+
+export (int) var day_length
+export (int) var dusk_length
+export (int) var dawn_length
+export (int) var night_length
+export (int) var event_length
+
+export (int) var trans_length
+#transition length between cycles
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+#var fullbright = Color(1, 1, 1)
+#var dusk = Color(0.407843, 0.407843, 0.407843)
+#var dawn = Color(0.54902, 0.54902, 0.54902)
+#var reddusk = Color(0.734375, 0.327026, 0.327026)
+#var purplemoon = Color(0.376471, 0.247059, 1)
+#future colors include rainbow and rgb flashing
 
 
 # Called when the node enters the scene tree for the first time.
@@ -14,3 +41,19 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+func _on_Timer_timeout():
+	#restart timer
+	#Color(0.407843, 0.407843, 0.407843) dusk
+	# dawn
+	#Color(1, 1, 1) full bright
+	# red dusk
+	# purple moon
+	
+	#color [default: Color( 1, 1, 1, 1 )]
+	#set_color(value) setter
+	#get_color() getter
+
+	#if 
+	pass # Replace with function body.
