59aa983c84a23e4302a329763a2dd0fab8f28bb8
MadeBySaints <MadeBySaints@users.noreply.github.com>
2020-11-05 23:47:08 -0500

    misc code hygiene

--- Core/Player/PlayerController.gd
+++ Core/Player/PlayerController.gd
@@ -5,19 +5,21 @@ export (int) var speed = 70
 var velocity = Vector2()
 var state_machine
 var facing_dir
-#var a = preload("res://Core/Scripts/EmoteBubble.gd")
 
 var emote = load("res://Core/Abstract Scenes/Emoji.tscn")#script
 var a = emote.instance()
 
 func _ready():
 	state_machine = $AnimationTree.get("parameters/playback")
+#	if facing_dir == null:
+	facing_dir = "down"
 	pass
 
 func get_input():
 	#initialize velocity so player doesnt run away
 	velocity = Vector2()
-	var current = state_machine.get_current_node()
+#	var current = state_machine.get_current_node()
+#	print(current)
 	
 	if Input.is_action_pressed("up"):
 		velocity.y -= 1
@@ -38,14 +40,17 @@ func get_input():
 	if velocity.length() == 0:
 		state_machine.travel("idle" + str(facing_dir))
 		
+		
 	velocity = velocity.normalized() * speed
+	
 	
 	if Input.is_action_just_pressed("attack"):
 		state_machine.travel("atk" + str(facing_dir))
-#	if Input.is_action_just_pressed("emote_radial"):
+		
+		
+	if Input.is_action_just_pressed("emote_radial"):
 		#display radial menu
-##		pass
-#	if a != null:
+		pass
 	if Input.is_action_just_pressed("emote1"):
 		a.happy()
 		add_child(a)
@@ -58,9 +63,8 @@ func get_input():
 	if Input.is_action_just_pressed("emote4"):
 		a.hate()
 		add_child(a)
-#	else:
-#		print(str(a) + "is running")
-
+		
+		
 func _physics_process(_delta):
 	get_input()
 	velocity = move_and_slide(velocity)
@@ -69,6 +73,4 @@ func _physics_process(_delta):
 func hurt():
 	pass
 
-#func emote():
-#	pass
 	
--- Core/Scripts/EmoteBubble.gd
+++ Core/Scripts/EmoteBubble.gd
@@ -1,45 +1,25 @@
 extends Node2D
 
-
-var a
-var iter = 0
-export (int) var max_iter = 3
-var process
-#set position
-
-func _init():
-	
-	pass
+var i = 0
+export (int) var max_i = 3
 
 func _ready():
 	pass
 
-#set animation name
 func happy():
 	$AnimatedSprite.play("happy")
-	
-	
 func angry():
 	$AnimatedSprite.play("angry")
-	
-	
 func love():
 	$AnimatedSprite.play("love")
-	
-	
 func hate():
 	$AnimatedSprite.play("hate")
 
-
-
-#queue free when complete
-
-
-
-# pseudo timer for animation - time controlled with speed_scale(1.0)
+# pseudo timer for anim. change max_i to change the length
+# of the anim. Lower numbers = shorter animation. Floats not supported.
 func _on_AnimatedSprite_animation_finished():
-	iter += 1
-	if iter >= max_iter:
-		iter = 0
+	i += 1
+	if i >= max_i:
+		i = 0
 		$AnimatedSprite.stop()
 		.get_parent().remove_child(self)
