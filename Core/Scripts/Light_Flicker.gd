5a7950ffc81eb289534d0cddbc1bc0de2a425172
Chris Hunsinger <hunsinger89@gmail.com>
2021-02-17 04:01:54 -0500

    lighting update along with minor additions to map

--- Core/Scripts/Light_Flicker.gd
+++ Core/Scripts/Light_Flicker.gd
@@ -1,38 +1,37 @@
 extends Light2D
 
-onready var sprite = get_parent().get_node("AnimatedSprite")
+export (bool) var isoff = false
+export (int) var nPer = 16
+export (float) var defEnergy = 0.8
+export (float) var flux = 0.5
+
 onready var noise = OpenSimplexNoise.new()
 var value = 0.0
 const MAX = 100000000
 
-
 func _ready():
 	randomize()
 	value = randi() % MAX
-	noise.period = 16
+	noise.period = nPer
 	set_process(true)
-
-
+	if isoff == true:
+		turn_off()
 
 func turn_off():
 	set_energy(0.0)
-	sprite.set_visible(false)
 	pass
 
 func turn_on():
-	set_energy(0.8)
-	sprite.set_visible(true)
+	set_energy(defEnergy)
 	pass
-
 
 func flicker():
 	var a = get_color()
-	value += 0.5
+	value += flux
 	if(value > MAX):
 		value = 0.0
 	var alpha = ((noise.get_noise_1d(value) + 1) / 4.0) + 0.5
 	self.color = Color(color.r, color.g, color.b, alpha)
-	
-	
+
 func _process(_delta):
 	flicker()
