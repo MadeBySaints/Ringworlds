38e93d082566ccadc4bc9dab4d7de82776a03649
Chris Hunsinger <hunsinger89@gmail.com>
2021-10-09 21:11:16 -0400

    updates to rain code

--- Core/Scripts/Day_Night_Cycle.gd
+++ Core/Scripts/Day_Night_Cycle.gd
@@ -9,8 +9,16 @@ var c_norm = {
 var c_spec = {
 	purp_moon = Color(0.376471, 0.247059, 1, 1)
 }
+var weather = {
+	clear = 0,
+	rain = 1,
+	heavy_rain = 2,
+	lightning = 3,
+	snow = 4
+}
 
 onready var current_cycle = c_norm.day#returns Color(n,n,n,n)
+onready var current_weather = weather.clear
 
 func _process(_delta):
 	if current_cycle == c_norm.day:
